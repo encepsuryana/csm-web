3e7dca4b5768e7a5e26c3a6db36cbaace14f8844
unknown <encep.suryanajr@gmail.com>
2019-08-29 13:33:34 +0700

    Patch Security Website

--- application/logs/log-2019-08-29.php
+++ application/logs/log-2019-08-29.php
@@ -5,3 +5,17 @@ ERROR - 2019-08-29 10:49:22 --> 404 Page Not Found!: Public/css
 ERROR - 2019-08-29 11:01:27 --> Severity: Notice --> Undefined variable: final_names C:\xampp\htdocs\csm-web\application\controllers\admin\Portfolio.php 395
 ERROR - 2019-08-29 11:01:27 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp\htdocs\csm-web\application\controllers\admin\Portfolio.php 395
 ERROR - 2019-08-29 11:24:12 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:56:48 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:58:20 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:58:41 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:58:47 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:58:55 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 11:59:05 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:01:00 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:03:38 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:03:48 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:03:54 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:03:57 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:05:02 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:05:06 --> 404 Page Not Found!: Public/css
+ERROR - 2019-08-29 13:05:17 --> 404 Page Not Found!: Public/css
