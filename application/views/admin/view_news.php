d4604a65aca376bb25fb234f71170921a1c06f4d
unknown <encep.suryanajr@gmail.com>
2019-09-09 17:54:18 +0700

    optimaze table-responsive

--- application/views/admin/view_news.php
+++ application/views/admin/view_news.php
@@ -20,7 +20,7 @@ if(!$this->session->userdata('id')) {
 		<div class="row">
 			<div class="col-md-12">
 				<div class="box box-info b-box">
-					<div class="box-body table-responsive" style="overflow-x: hidden;">
+					<div class="box-body table-responsive">
 						<table id="example1" class="table table-bordered table-striped">
 							<thead>
 								<tr>
