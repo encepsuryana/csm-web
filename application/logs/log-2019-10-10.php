22a258ee06a4e5f8bf53fcea5b736119fe2b891d
Encep Suryana McCarley <encep.suryanajr@gmail.com>
2019-10-10 15:58:10 +0700

    Update UI admin panel

---
+++ application/logs/log-2019-10-10.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-10 15:54:05 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:54:31 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:54:33 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:54:36 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:55:30 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:55:35 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:55:44 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:56:41 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:57:14 --> 404 Page Not Found: Public/admin
+ERROR - 2019-10-10 15:57:35 --> 404 Page Not Found: Public/admin
